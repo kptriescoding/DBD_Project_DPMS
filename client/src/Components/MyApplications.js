c2190f3892f8ec81b8287005e68ff23bc625a676
Dileep Sharma <84862839+dileep-shar@users.noreply.github.com>
2023-01-29 23:44:56 +0530

    c#7

--- client/src/Components/MyApplications.js
+++ client/src/Components/MyApplications.js
@@ -1,36 +1,80 @@
-import { async } from '@firebase/util'
-import React from 'react'
+import { async } from "@firebase/util";
+import React from "react";
 
 export default function MyApplications(props) {
-    const [applications, setapplications] = useState([])
+  const [applications, setapplications] = useState([]);
 
-    function getAllicationCards(){
+  async function handleSetApplications() {}
+  async function handleDeleteApplications() {}
 
+  async function handleAcceptOrRejectApplications(
+    projectId,
+    Student_Email,
+    isAccept
+  ) {}
+  useEffect(() => {
+    handleSetApplications();
+  }, []);
+
+  const StudentsApplied = () => {
+    {
+      /* @todo Professor side : student name year cgpa  accept reject*/
     }
-    async function handleSetApplications(){
 
-    }
-    async function handleDeleteApplications(){
+    const students = applications.map((student) => {
+      return (
+        <Card
+          isPressable
+          isHoverable
+          variant="bordered "
+          style={{
+            width: "inherit",
+            borderRadius: "0.6rem",
+            margin: "1.5px",
+          }}
+          key={proj.projectId}
+        >
+          <Card.Header
+            css={{
+              backgroundColor: curColor,
+            }}
+          >
+            <Text
+              style={{
+                color: "#ffffff",
+              }}
+            >
+              {student.studentName}
+            </Text>
+          </Card.Header>
+          <Card.Divider
+            style={{
+              backgroundColor: "gray",
+            }}
+          />
+          <Card.Body>
+            <Text>{proj.projectDescription}</Text>
+          </Card.Body>
+          <Card.Divider />
+          <Card.Footer style={{ justifyContent: "end" }}>
+            <Text small style={{ justifyContent: "end" }}>
+              {proj.collaborator}
+            </Text>
+          </Card.Footer>
+        </Card>
+      );
+    });
+  };
 
-    }
-
-    async function handleAcceptOrRejectApplications(projectId,Student_Email,isAccept){
-
-    }
-    useEffect(() => {
-      handleSetApplications()
-    
-    }, [])
-    
   return (
     <div>
       <div>
-        <span className=' font-bold text-2xl'>Applications</span>
+        <span className=" font-bold text-2xl">Applications</span>
         <div>
-{/* @todo Professor side : student name year cgpa  accept reject*/}
-
+          {/* @todo Professor side : student name year cgpa  accept reject*/}
+          {/* student side:project name remove application  */}
         </div>
       </div>
     </div>
-  )
+  );
 }
